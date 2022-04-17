44ab6696e24e62c2f5b743337eec8739cef11bf2
Hosein Abbaspour <abspr@Hoseins-Mac.local>
2022-04-17 18:33:18 +0300

    Demo project done, ready for first release

--- Sources/TFManager/TFManager.swift
+++
@@ -1,6 +0,0 @@
-public struct TFManager {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
--- TFManagerDemo/TFManagerDemo/ViewController.swift
+++ TFManagerDemo/TFManagerDemo/ViewController.swift
@@ -6,14 +6,34 @@
 //
 
 import UIKit
+import TFManager
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    
+    var form = TFManager()
+    
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var ageField: UITextField!
+    
+    @IBOutlet weak var mailField: ValidatableField! {
+        didSet {
+            mailField.rulesRepo.add(RulesSet.mail)
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        form.add([nameField, mailField, ageField])
+        form.delegate = self
     }
-
 
 }
 
+extension ViewController: FormDelegate {
+    
+    func textDidChange(_ textField: UITextField, validationResult: ValidationResult?) {
+        guard let validationResult = validationResult else { return }
+        textField.textColor = validationResult.isValid ? .label : .systemRed
+    }
+}
